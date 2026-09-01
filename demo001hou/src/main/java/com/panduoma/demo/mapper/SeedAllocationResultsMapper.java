3314bb48888a284d320ef179a315d1871d653ebc
g-g23456 <256880141@qq.com>
2026-09-01 16:58:00 +0800

    改

---
+++ demo001hou/src/main/java/com/panduoma/demo/mapper/SeedAllocationResultsMapper.java
@@ -0,0 +1,9 @@
+package com.panduoma.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.panduoma.demo.entity.SeedAllocationResults;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SeedAllocationResultsMapper extends BaseMapper<SeedAllocationResults> {
+}
---
+++ demo001hou/src/main/java/com/panduoma/demo/mapper/WaterAnalysisMapper.java
@@ -0,0 +1,9 @@
+package com.panduoma.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.panduoma.demo.entity.WaterAnalysis;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface WaterAnalysisMapper extends BaseMapper<WaterAnalysis> {
+}
